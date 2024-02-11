257aaf8365406a3c3ad610b9688809b049be034c
Glitch (main-game-page) <none>
2024-02-11 02:46:30 +0000

    🐋🙇 Checkpoint ./style.css:62997810/169 ./index.html:62997810/179 ./script.js:62997810/209

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ document.getElementById("MainSelectors").style.display = "none"
 document.getElementById("H1").style.display = "none"
 function AddList(){
   document.getElementById("btnChem").addEventListener("click",Chem)
+  document.getElementById("btnGame1Submit").addEventListener("click",Submit1)
 }
 
 function Chem(){
   document.getElementById("MainSelectors").style.display = "none"
   document.getElementById("H1").style.display = "none"
+}
+
+function Submit1(){
+  ans1 = document.getElementById("MainSelectors").value
 }
